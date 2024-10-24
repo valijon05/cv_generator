902fa3bac7a38b34781f2fad35fc5cd7f242c336
valijon05 <valijoneshmamatov98@gmail.com>
2024-10-24 20:42:27 +0500

    unit testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,25 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
     /**
      * A basic test example.
      */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+    }
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
     }
+
 }
