e2ccf67a2fcd422c57c7324cc84c30b589e50710
valijon05 <valijoneshmamatov98@gmail.com>
2024-10-25 20:26:21 +0500

    Added deployment script for production environment

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,18 @@
 <?php
-//
-//namespace Tests\Unit;
-//
-//use PHPUnit\Framework\Attributes\Test;
-//use PHPUnit\Framework\TestCase;
-//
-//class ExampleTest extends TestCase
-//{
-//    /**
-//     * @test
-//     */
-//    #[Test]
-//    public function test_that_true_is_true(): void
-//    {
-//        $this->assertTrue(true);
-//    }
-//}
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    #[Test]
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
